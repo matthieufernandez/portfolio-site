7aa96df2c0f4a43143cda6526cad6dafa7d3103f
matt <mrmatthieufernandez@gmail.com>
2023-09-12 13:56:20 -0400

    added all custom project images

--- app/lib/projects.ts
+++ app/lib/projects.ts
@@ -1,38 +1,23 @@
 export const projects: Projects = [
+    
     {
         id: 1,
-        name: "Pokemon Project",
+        name: "Gestion Horizon",
         description: "Something something something",
-        link: "https://github.com/matthieufernandez/pokemon-project-personal",
-        img: "/images/project-images/pokemon.png",
+        link: "https://github.com/matthieufernandez/gestion-horizon",
+        img: "/images/project-images/GH.png",
         video: null
     },
     {
         id: 2,
-        name: "Gestion Horizon",
+        name: "Pokemon Project",
         description: "Something something something",
-        link: "https://github.com/matthieufernandez/gestion-horizon",
-        img: "/images/project-images/gestion-horizon.png",
+        link: "https://github.com/matthieufernandez/pokemon-project-personal",
+        img: "/images/project-images/pkmn.png",
         video: null
     },
     {
-        id: 3, 
-        name: "REST game",
-        description: "Something something something",
-        link: "https://github.com/matthieufernandez/rest-game",
-        img: "/images/project-images/REST.png",
-        video: null
-    },
-    {
-        id: 4,
-        name: "DVD Library",
-        description: "Something something something",
-        link: "https://github.com/matthieufernandez/dvd-library",
-        img: "/images/project-images/dvdlib.jpg",
-        video: null
-    },
-    {
-        id: 5,
+        id: 3,
         name: "Foodie Friend",
         description: "Something something something",
         link: "https://github.com/matthieufernandez/foodie-friend",
@@ -40,11 +25,28 @@ export const projects: Projects = [
         video: null
     },
     {
+        id: 4, 
+        name: "REST game",
+        description: "Something something something",
+        link: "https://github.com/matthieufernandez/rest-game",
+        img: "/images/project-images/REST.png",
+        video: null
+    },
+    {
+        id: 5,
+        name: "DVD Library",
+        description: "Something something something",
+        link: "https://github.com/matthieufernandez/dvd-library",
+        img: "/images/project-images/dvdlib.png",
+        video: null
+    },
+   
+    {
         id: 6,
         name: "Wikisearch",
         description: "Something something something",
         link: "https://github.com/matthieufernandez/nextjs-training",
-        img: "/images/project-images/wiki-search.png",
+        img: "/images/project-images/WIKISEARCH.png",
         video: null,
     }
 ]
